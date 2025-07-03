5754d6f08afc1b634ee45c5e962e8363f41c235e
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2025-07-03 16:43:41 +0700

    master

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -11,7 +11,6 @@ interface MongooseGlobal {
   promise: Promise<typeof mongoose> | null;
 }
 
-// Extend NodeJS.Global with mongoose property
 declare global {
   // eslint-disable-next-line no-var
   var mongoose: MongooseGlobal | undefined;
@@ -24,12 +23,12 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  if (cached!.conn) return cached!.conn;
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
 
 export default dbConnect;
